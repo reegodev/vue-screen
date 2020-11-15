ea4f2da19b39515e43688fa1325b83fc4cf93240
Matteo Rigon <matteo.rigon7@gmail.com>
2020-11-15 22:17:20 +0100

    feat(demo): use current version in dist directory

--- demo/src/main.js
+++ demo/src/main.js
@@ -5,25 +5,6 @@ import App from './App.vue';
 Vue.use(VueScreen, {
   extend: 'tailwind',
   mobileLandscape: screen => screen.landscape && screen.touch,
-  breakpoint(screen) {
-    if (screen.xl) {
-      return 'xl';
-    }
-
-    if (screen.lg) {
-      return 'lg';
-    }
-
-    if (screen.md) {
-      return 'md';
-    }
-
-    if (screen.sm) {
-      return 'sm';
-    }
-
-    return 'xs';
-  },
   device(screen) {
     if (screen.xl) {
       return 'Widescreen';
--- demo/vue.config.js
+++ demo/vue.config.js
@@ -1,6 +1,15 @@
+const path = require('path')
+
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/vue-screen/'
     : '/',
   productionSourceMap: false,
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'vue-screen': path.resolve(__dirname, '../dist/vue-screen.esm.js')
+      }
+    }
+  }
 };
